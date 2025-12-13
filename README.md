31600f202855edc0b58b29a0e2f64f0d1072860d
marcozanon1 <166063879+marcozanon1@users.noreply.github.com>
2025-12-13 14:18:13 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews-
-Treinando 
+
+Treinando
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Te amo, Evellyn. Minha bonequinha linda *cabeça concordando*</h1>
+function Home() {
+  return <h1>Te amo, Evellyn. Minha bonequinha linda *cabeça concordando*</h1>;
 }
 
 export default Home;
